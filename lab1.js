c0599ccf51762f61c7260d15649a8eaf4a88ac75
JeroenFaasse <99045518@mydavinci.nl>
2018-10-16 10:56:54 +0200

    finished lab 1, 2

---
+++ lab1.js
@@ -0,0 +1,9 @@
+var aantal = prompt('Bedenk een getal', 'Voer hier je getal in');
+
+function helloWorld() {
+	for (var i = 0; i <aantal; i++) {
+		document.write('Hello World' + '<br>');
+	}
+}
+
+helloWorld();
---
+++ lab2.js
@@ -0,0 +1,65 @@
+
+
+function bereken() {
+	var num1, num2, result;
+
+	 function getNum1(){
+        num1 = prompt("Voer hier je eerste getal in");
+        return num1;
+    }
+
+    function getNum2(){
+        num2 = prompt("Voer hier je tweede getal in");
+        return num2;
+    }
+
+    function getOperator(){
+        var operator = prompt("Voer hier je operator in");
+        return operator;
+    }
+
+    function setResult(){
+        if(!alert("Result = " + result)){window.location.reload();}
+    }
+
+
+	var num1 = getNum1();
+	var num2 = getNum2();
+	var decrement = Number(num1) + Number(num2);
+	var increment = Number(num1) + Number(num2);
+	var operator = getOperator();
+
+	if (operator == "+", "plus") {
+
+		result = Number(num1) + Number(num2);
+
+	}else if(operator == "*", "keer") {
+
+	    result = Number(num1) * Number(num2);
+
+	}else if(operator == "/", ":", "delen") {
+
+	    result = Number(num1) / Number(num2);
+
+	}else if (operator == "-", "min") {
+
+	    result = Number(num1) - Number(num2);
+
+	}else if (operator == "++") {
+		
+		result = (increment++);
+		result = increment;
+
+	}else if(operator == "--"){
+	
+		result = (decrement--);
+		result = decrement;
+
+	}
+
+	setResult();
+}
+
+bereken();
+
+
