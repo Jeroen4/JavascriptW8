0690e7ef6831ba5018ebb4125fb2833032eec25f
JeroenFaasse <99045518@mydavinci.nl>
2018-11-20 13:32:42 +0100

    fixed bitterbal order

--- horecaApp.js
+++ horecaApp.js
@@ -15,9 +15,11 @@ var schaal16 = 0;
 
 var input;
 
-while (input != ("stop")){
+addToOrder();
 
-// hier worden de vragen gesteld en de antwoorden gelogd, de antwoorden worden opgeslagen in de VARIABELEN
+function addToOrder(){
+	while (input != "stop"){
+	// hier worden de vragen gesteld en de antwoorden gelogd, de antwoorden worden opgeslagen in de VARIABELEN
 	input = prompt("wat wilt u bestellen?");
 	if (input == "fris") {
 		var newFris = parseInt(prompt("hoeveel fris wilt u bestellen?"));
@@ -34,36 +36,55 @@ while (input != ("stop")){
 		wijn = wijn + newWijn;
 		console.log(wijn , "x" , PRICEWIJN);
 	}
-	else if (input == "snack"){
-                var newSchaal8 = parseInt(prompt("kies hoeveel schalen van 8 u wilt."));
-                schaal8 = schaal8 + newSchaal8;
-                var newSchaal16 = parseInt(prompt("kies hoeveel schalen van 16 u wilt."));
-                schaal16 = schaal16 + newSchaal16;
-                console.log(schaal8, "x" , PRICESNACK8); 
-                console.log(schaal16, "x" , PRICESNACK16);
+	 else if (input == "snack"){
+	 	inputSnack = prompt("Hoeveel wilt u?", "8 of 16")
+	 	if (inputSnack == "8") {
+	 		var newSchaal8 = parseInt(prompt("kies hoeveel schalen van 8 u wilt."));
+            schaal8 = schaal8 + newSchaal8;
+            console.log(schaal8, "x" , PRICESNACK8);
             }
-    
+            else if (inputSnack == "16"){
+	            var newSchaal16 = parseInt(prompt("kies hoeveel schalen van 16 u wilt."));
+	            schaal16 = schaal16 + newSchaal16;
+	            console.log(schaal16, "x" , PRICESNACK16);
+			}
+        }
+    }
+}
+calcPrice();
+	
+	//Deze functie rekent het totaal bedrag uit.
+function calcPrice(){
+	var frisPrice = fris*PRICEFRIS;
+	var bierPrice = bier*PRICEBIER; 
+	var wijnPrice = wijn*PRICEWIJN;					//variabelen met alle prijzen.
+	var snackPrice8 = schaal8*PRICESNACK8;
+	var snackPrice16 = schaal16*PRICESNACK16;
+	var totalPrice = frisPrice+bierPrice+wijnPrice+snackPrice8+snackPrice16;		
+	if (fris > 0) {
+		document.write("U heeft"," ", fris," ", "fris.", " ","€", frisPrice);
+		document.write("<br><br>");
+	}
+	if (bier > 0) {
+		document.write("U heeft"," ", bier," ", "bier.", " ","€", bierPrice);
+		document.write("<br><br>");	
+	}
+	if (wijn > 0) {
+		document.write("U heeft"," ", wijn," ", "wijn.", " ","€", wijnPrice);
+		document.write("<br><br>");  		//"<br><br>" staat voor een dubbele ENTER.
+	}
+	if (schaal8 > 0) {
+		document.write("U heeft"," ",schaal8," ", "schalen van 8 bitterballen.", " ", "€", snackPrice8);
+		document.write("<br><br>");
+	}
+	if (schaal16 > 0) {
+		document.write("U heeft"," ",schaal16," ", "schalen van 16 bitterballen."," ", "€", snackPrice16);
+		document.write("<br><br>");	
+	}
+	document.write("De totale prijs is €", totalPrice);
 }
 
 
 
-
-var frisPrice = fris*PRICEFRIS;
-var bierPrice = bier*PRICEBIER; 
-var wijnPrice = wijn*PRICEWIJN;
-var snackPrice8 = schaal8*PRICESNACK8;
-var snackPrice16 = schaal16*PRICESNACK16;
-var totalPrice = frisPrice+bierPrice+wijnPrice+snackPrice8+snackPrice16;		//hier word het totaal bedrag uitgerekent.
-
-/*Prijsweergave*/
-document.write("U heeft"," ", fris," ", "fris.", " ","€", frisPrice);
-document.write("<br><br>");		
-document.write("U heeft"," ", bier," ", "bier.", " ","€", bierPrice);
-document.write("<br><br>");					//"<br><br>" staat voor een dubble ENTER.
-document.write("U heeft"," ", wijn," ", "wijn.", " ","€", wijnPrice);
-document.write("<br><br>");		
-document.write("U heeft"," ",schaal8," ", "schalen van 8 bitterballen.", " ", "€", snackPrice8);
-document.write("<br><br>");		
-document.write("U heeft"," ",schaal16," ", "schalen van 16 bitterballen."," ", "€", snackPrice16);
-document.write("<br><br>");	
-document.write("<br><br>", "De totale prijs is €", totalPrice);
+		
+		
